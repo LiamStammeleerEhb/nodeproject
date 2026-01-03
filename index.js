5668a103bef95684a3583011b600c76bb4740397
LiamStammeleerEhb <liam.stammeleer@student.ehb.be>
2026-01-03 13:54:24 +0100

    Test router

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import session from 'express-session';
+import path from "path";
+
 const app = express();
 
-app.use(express.static("pages"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.resolve("pages/index.html"));
+    
+});
+
+app.get('/login', (req, res) => {
+    res.send("Login page works");
+    res.sendFile(path.resolve("pages/login.html"));
 });
 
 app.listen(3000, () => {
